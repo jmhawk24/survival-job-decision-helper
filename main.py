429cb15fff260fb6efa2fbc17d7d82513d5d0394
Jacob Hawk <hawk.music24@gmail.com>
2023-02-22 08:01:47 -0700

    first draft good to go

--- main.py
+++ main.py
@@ -2,6 +2,21 @@
 
 # maybe I could use redis in this to store the last few run results, export them or something.
 
+def is_worth_it(surv_wage, real_wage, months_offset_with_surv, months_offset_without_working):
+    is_full_time = input('Would you be working full-time? Type `y` if so.') == 'y'
+    if is_full_time:
+        monthly_surv_wage = surv_wage * 8 * 22
+    else:
+        monthly_surv_wage = surv_wage * 8 * 11
+
+    monthly_real_wage = real_wage * 8 * 22
+
+    total_wages_with_surv = monthly_surv_wage * months_offset_with_surv
+    total_wages_without_surv = monthly_real_wage * (months_offset_with_surv - months_offset_without_working)
+    result = total_wages_with_surv > total_wages_without_surv
+
+    return [result, total_wages_with_surv, total_wages_without_surv]
+
 
 def get_intersect(surv_wage, real_wage, months_offset):
     # mx + b for now_line: now_linex
@@ -17,9 +32,6 @@ def get_intersect(surv_wage, real_wage, months_offset):
 
 
 def get_intersects(surv_wage, real_wage, months_offset):
-    # for each month offset, calculate intersect of two lines
-    # if surv is 11/hr, and the graph is dollars over months of time
-    # convert 11 into monthly wage
     monthly_survival_pt = surv_wage * 8 * 11
     monthly_survival_ft = surv_wage * 8 * 22
     monthly_real_ft = real_wage * 8 * 22
@@ -40,19 +52,34 @@ def main():
             input('Now, not counting benefits, how much would you expect to be paid for the "real" job?\n'))
 
         expected_time_to_find_real_job_while_working = int(
-            input('How long do you think it will take you to find a real job?'))
+            input('How long do you think it will take you to find a real job if you had a survival job?'))
         expected_time_to_find_real_job_unemployed = int(
             input('Now, how long do you expect it would take to you find a real job if you weren\'t working?'))
 
-        intersects = get_intersects(expected_survival_wage, expected_real_wage,
-                                    expected_time_to_find_real_job_while_working)
-        print('''If it takes you + {expected_time} to find a "real" job, 
-        then your real job wages will pass the survival job wages {intersect_months_from_start} months after starting your real job, 
-        or {intersect_months_from_now} months from now.'''
-              .format(expected_time=expected_time_to_find_real_job_while_working,
-                      intersect_months_from_start=intersects[0] - expected_time_to_find_real_job_while_working,
-                      intersect_months_from_now=intersects[0]))
-        print('Remember that this calculation does not count benefits! It assumes 40 hrs / week.')
+        res, wages_with, wages_without = is_worth_it(expected_survival_wage,
+                                                     expected_real_wage,
+                                                     expected_time_to_find_real_job_while_working,
+                                                     expected_time_to_find_real_job_unemployed)
+        if res:
+            print('''it is worth it for you to get a survival job!''')
+            comp = 'more'
+        else:
+            print('It is not worth it for you to get a survival job.')
+            comp = 'less'
+
+        print('''You would earn {res} money over {months_with} months with a survival job
+        than you would by waiting and focusing on finding a real job.
+        Over {months_with} months, the survival job would earn you ${wages_with}.
+        Over {months_with} months, the real job would earn you ${wages_without},
+        assuming it took you {months_without} to find a real job.
+        '''.format(
+            res=comp,
+            months_with=expected_time_to_find_real_job_while_working,
+            months_without=expected_time_to_find_real_job_unemployed,
+            wages_with=wages_with,
+            wages_without=wages_without
+        ))
+        print('Remember that this calculation does not count benefits!')
         keep_going = input('Keep going? Type `y` to continue.')
         if not (keep_going == "y"):
             running = False
